70aef911d8a030c5f85a48f9ed8560b6f739f15d
Hani <Hani@Hanis-MacBook-Air.local>
2015-11-15 10:48:37 +0100

    One test

---
+++ test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+describe 'Random' do
+  describe 'when comparing equal numbers' do
+    it 'must respond positively' do
+      1.must_equal 1
+    end
+  end
+
+  describe 'when comparing non-equal numbera' do
+    it 'must respond negatively' do
+      1.must_equal 2
+    end
+  end
+end
